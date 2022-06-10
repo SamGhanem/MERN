f308ce185e780c8f0312c77af5fd4fed5e589ede
SamGhanem <yambo78@outlook.com>
2022-06-10 15:27:49 -0400

    commit

---
+++ First_jsx/client/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="Head">
+        Hello Dojo
+      </h1>
+      <h2 className="Head2">
+        Things I need to do:
+      </h2>
+      <ul>
+        <li>GOTO THE STORE</li>
+        <li>NEED A OIL CHANGE</li>
+        <li>steal the declaration independence</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;

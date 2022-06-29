934bcfc5a48b270bac79bf36c673b5f59705114d
SamGhanem <yambo78@outlook.com>
2022-06-29 15:22:10 -0400

    commit

---
+++ pokemon/client/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import axios from "axios";
+import React, {useEffect, useState} from "react"
+
+function App() {
+  const [pokemon,setPokemon] =useState([]);
+  useEffect(() => {
+    axios
+    .get("https://pokeapi.co/api/v2/pokemon/?limit=807")
+    .then((response) =>{
+      console.log(response.data.results);
+      setPokemon(response.data.results);
+    })
+      
+    .catch(err => console.log(err));
+  }, []);
+  return (
+    <div className="App">
+      <h1>Pokemon API</h1>
+      <ul>
+        {pokemon.map((pokemon, i) =>(
+        <li className='fun' key={i}>{pokemon.name}</li>
+        ))}
+      </ul>
+      
+    </div>
+  );
+}
+
+export default App;
